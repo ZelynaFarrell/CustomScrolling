8f93d77a3b6cdf93b4264029bb8315c43169e4e6
Zelyna Farrell <zsillas@yahoo.com>
2023-09-19 15:05:36 -0700

    Initial Commit

---
+++ CustomScrolling/CustomScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomScrollingApp.swift
+//  CustomScrolling
+//
+//  Created by Zelyna Sillas on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
